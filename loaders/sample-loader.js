b14008e25fc07858e2f767b7dc853bc7d4c13886
Hiruevan <98787662+hiruevan@users.noreply.github.com>
2024-08-06 17:02:56 -0600

    Update sample-loader.js
    Add "loc=" functionality for tutorials to reference

--- loaders/sample-loader.js
+++ loaders/sample-loader.js
@@ -10,14 +10,20 @@ function gw_loadFile(filePath) {
 }
 
 window.onload = function() {
-    if (window.location.search.length > 8) {
-        let projectToLoad = "";
+    let projectToLoad = "";
+    if (window.location.search.length > 8 && window.location.search.includes("sample=")) { // Sample based project load
+        projectToLoad = "./samples/"
         for (let i = 8; i < window.location.search.length; i++) {
             projectToLoad += window.location.search[i];
         }
-
-        const fileContent = gw_loadFile("./samples/" + projectToLoad + ".gw1");
-
+        projectToLoad += ".gw1";
+    } else if (window.location.search.length > 5 && window.location.search.includes("loc=")) { // Location based project load
+        for (let i = 5; i < window.location.search.length; i++) {
+            projectToLoad += window.location.search[i];
+        }
+    }
+    if (projectToLoad.length > 1) { // Load project
+        const fileContent = gw_loadFile(projectToLoad);
         gw_loadProjectFile(fileContent);
     }
 }
